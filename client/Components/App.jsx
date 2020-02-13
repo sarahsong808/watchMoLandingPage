ea7cf2f5cb58cf41f125e731db35c2c162a01bcc
Sarah's Mac <sarahsmac@Apples-Mac-4.local>
2020-02-13 00:00:30 -0800

    Updated features on styling and mobile compatibility

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -7,22 +7,48 @@ function App(props) {
         <section className="outerContainer">
 <a class="github-button" href="https://github.com/oslabs-beta/watchmo" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star oslabs-beta/watchmo on GitHub">Star</a>
 
-            <div>
+         
                 <img className="banner" src="/cat.png" />
 
                 <h1>
                     A server-side resolver response time detection tool for monitoring the health status of your GraphQL architecture.
                         The tool can be ran in the background on a server or a client to observe response times as the GraphQL database matures.
             </h1>
+            <div className="npmDiv"><button className="npm">INSTALL NPM</button></div>
+            <div className="info">
+                
+            <div>
+            <div className="video1">
+                <div className="caption2"></div>
+                <h3>MONITOR</h3>
+                <p>Seamlessly gather data from your server or a specific Graph-Ql endpoint with a single command.</p> 
+
+            </div>
+            <div className="video2">
+                <div>
+                 <video width="800" height="800" autoPlay loop><source src="Mov1.mp4" type="video/mp4" /></video>
+                </div> 
+                <div className="caption1"> 
+                <h3>ANALYZE</h3>
+                <p> Examine response times of your queries in simple but effective visualizations. </p>
+
+                </div> 
+            </div>
+            <h3>CONFIGURE</h3>
+            <p>Customize your Watchmo project through our easy to use GUI or CLI tools</p> 
+            </div>
+            <div className="devTeam">
+                
                 <h2>
-            The Development Team
+                    
+           The Development Team -- APIsomorphic
                 </h2>
                 <div className="headshots">  
              
-                    <img src="/Evan.png" className="headshot" id="evan"/>
-                    <img src="/Jason.png" className="headshot" id="jason"/>
-                    <img src="/Sarah.png" className="headshot" id="sarah"/>
-                    <img src="/Spencer.png" className="headshot" id="spencer"/>
+                    <img src="/evan.png" className="headshot" id="evan"/>
+                    <img src="/jason.png" className="headshot" id="jason"/>
+                    <img src="/sarah.png" className="headshot" id="sarah"/>
+                    <img src="/spencer.png" className="headshot" id="spencer"/>
 
                 </div>
                 <br />
@@ -85,9 +111,13 @@ function App(props) {
                             />
                         </a>
                         </div>
+                        </div>
                 </div>
             </div>
             <br/>
+            <div className="beta">watchMo is in beta. Please post any issues to our <a href="https://github.com/oslabs-beta/watchmo">Github</a> - we would love to hear from you!
+
+</div>
         </section>
 
     )
